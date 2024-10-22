27c63f5c9f5de7fe7a375194fd9594e48fd1b1dc
Kira-Master <fauziponcol21@gmail.com>
2024-10-23 05:17:26 +0700

    M

--- commands/panel/list.js
+++ commands/panel/list.js
@@ -10,7 +10,7 @@ module.exports = {
     callback: async ({ msg, client, args }) => {
     try {
     const body = {
-    let api_id = '45509'
+    let api_id = '45509',
     let api_key = '3dcay0-32levh-a921xp-kjoqny-qkgneh'
     }
     let res = await axios.post('https://www.irvankedesmm.co.id/api/services', body, { headers: { 'Content-Type' : 'application/json' } } )
