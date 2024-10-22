253bc4cb9e402e340c163cca1654e310f8d58a9f
Kira-Master <fauziponcol21@gmail.com>
2024-10-23 05:11:35 +0700

    M

---
+++ commands/panel/list.js
@@ -0,0 +1,18 @@
+const axios = require('axios')
+
+module.exports = {
+    category: 'Owner',
+    description: 'Add premium user',
+    ownerOnly: true,
+    minArgs: 2,
+    expectedArgs: '<number> <date>',
+    example: '{prefix}{command} 62xxxx',
+    callback: async ({ msg, client, args }) => {
+    try {
+    let res = await axios.post('https://www.irvankedesmm.co.id/api/services', { 45509, '3dcay0-32levh-a921xp-kjoqny-qkgneh' }, { headers: { 'Content-Type' : 'application/json' } }
+    console.log(res.data)
+    } catch(error) {
+    console.error(error)
+    }
+    }
+}
