e6ad3a2df7c6c85be9ee8d7cd0fdbbf59a9bc6dd
Kira-Master <fauziponcol21@gmail.com>
2024-10-23 02:28:09 +0700

    YES

--- commands/searcher/modapk.js
+++ commands/searcher/modapk.js
@@ -21,7 +21,7 @@ module.exports = {
     zexxaMsg += `*Title: ${i.title}*\n`
     zexxaMsg += `*Link: ${i.link}*\n`
     }
-    await msg.replyImage({ url: i.thumb }, zexxaMsg).catch(() => 
+    await msg.replyImage({ url: xxx.result.thumb }, zexxaMsg).catch(() => 
     { return msg.reply('error saat mengirim data')
     })
     }
