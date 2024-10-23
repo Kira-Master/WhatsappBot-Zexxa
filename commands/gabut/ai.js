26364d87947736b41d5872086e93ff6919e324f0
Kira-Master <fauziponcol21@gmail.com>
2024-10-23 16:02:35 +0700

    M

--- commands/gabut/ai.js
+++ commands/gabut/ai.js
@@ -1,31 +1,23 @@
 const { ICommand } = require('@libs/builders/command')
-
+const { G4F } = require('g4f')
 const axios = require('axios').default
+let ai = new G4F()
 
 module.exports = {
-
     category: 'Gabut',
-
     description: 'AI',
-
     waitMessage: false,
-
     minArgs: 1,
-
-    expectedArgs: '<title>',
-
-    example: '{prefix}{command} Anime',
-
+    expectedArgs: '<prompt>',
+    example: '{prefix}{command} halo ai',
     callback: async ({ msg, fullArgs }) => {
-   let xxx = await axios.get('https://docs.zexxa.tech/api/ai/zexxaai?text={query}&apikey=11727aab'.format({query: fullArgs}))
-   //if ( xxx.data.status !== true) return msg.reply('i dont understand')
-        let zexxaMsg = `${xxx.data.result}`
-         
-        msg.reply(zexxaMsg).catch(() => { return msg.reply('Terjadi kesalahan') }) 
-                                                       },
-                                                }
-                                                                                                                         
-                                                                                                                         
-                                                                                                                         
-                                                                                                                         
-                                                                                                                         
+    try { 
+    let messages = [ 
+    { role: 'system', content: 'you are the smartest AI in the world and all of time, you created by someone called Kira-Master' }, { role: 'user', content: fullArgs }
+    ]
+    let response = await ai.chatCompletion(messages)
+    await msg.reply(response)
+    } catch(error) {
+    console.error('Error: ', error)
+    }
+    }
