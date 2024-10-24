f6f81f0986f3af511ede1e45bac5296f3f05602e
Kira-Master <fauziponcol21@gmail.com>
2024-10-24 13:33:05 +0700

    UPDATE

--- libs/scraper/lk21.js
+++ libs/scraper/lk21.js
@@ -22,7 +22,7 @@ const lk21 = async (text) => {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
   },
   proxy: {
-        host: ip,,
+        host: ip,
         port: parseInt(port)
         }   
 });
