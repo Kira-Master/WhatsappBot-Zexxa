925660881680cffa79223ed6ab6dcb315201d914
Kira-Master <fauziponcol21@gmail.com>
2024-10-24 14:52:50 +0700

    m

--- libs/scraper/lk21.js
+++ libs/scraper/lk21.js
@@ -19,6 +19,7 @@ const lk21 = async (text) => {
     });
 
     const config = {
+    proxy,
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
