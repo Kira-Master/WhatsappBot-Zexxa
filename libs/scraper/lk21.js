8f732d436cc8a03b5176ff460bdfcd1549bb13b5
Kira-Master <fauziponcol21@gmail.com>
2024-10-24 13:22:24 +0700

    UPDATE

--- libs/scraper/lk21.js
+++ libs/scraper/lk21.js
@@ -6,7 +6,11 @@ const lk21 = async (text) => {
     const baseUrl = 'https://tv4.lk21official.my';  // Tambahkan domain
 
     // Mengambil halaman pencarian
-    const { data } = await axios.get(`${baseUrl}/search.php?s=${encodeURIComponent(text)}&gsc.tab=0&gsc.q=${encodeURIComponent(text)}&gsc.page=1`);
+    const { data } = await axios.get(`${baseUrl}/search.php?s=${encodeURIComponent(text)}&gsc.tab=0&gsc.q=${encodeURIComponent(text)}&gsc.page=1`, {
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+  }
+});
     
     const $ = cheerio.load(data);
     const scrape = [];
